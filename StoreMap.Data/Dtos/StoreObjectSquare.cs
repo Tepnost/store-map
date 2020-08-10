2cd31762b386293292b85496aaf1df19e5008500
Nerijus Dulke <dnerijus72@gmail.com>
2020-08-10 17:19:48 +0300

    fix rect shape drawing

--- StoreMap.Data/Dtos/StoreObjectSquare.cs
+++ StoreMap.Data/Dtos/StoreObjectSquare.cs
@@ -1,16 +1,72 @@
-﻿namespace StoreMap.Data.Dtos
+﻿using System;
+
+namespace StoreMap.Data.Dtos
 {
     public class StoreObjectSquare : StoreObject
     {
         public int Width { get; set; }
         public int Height { get; set; }
+        private bool isInversedX;
+        private bool isInversedY;
 
         public override string GetShapeStyles => $"width: {Width}px; height: {Height}px;";
         
         public override void UpdateSize(int currentX, int currentY)
         {
-            Width = currentX - X;
-            Height = currentY - Y;
+            SetX(currentX);
+            SetY(currentY);
+        }
+
+        private void SetX(int currentX)
+        {
+            var w = currentX - X;
+
+            if (w < 0)
+            {
+                var absW = Math.Abs(w);
+                X -= absW;
+                Width += absW;
+                isInversedX = true;
+            }
+            else
+            {
+                if (isInversedX && currentX < X + Width)
+                {
+                    Width -= currentX - X;
+                    X = currentX;
+                }
+                else
+                {
+                    isInversedX = false;
+                    Width = w;
+                }
+            }
+        }
+
+        private void SetY(int currentY)
+        {
+            var h = currentY - Y;
+
+            if (h < 0)
+            {
+                isInversedY = true;
+                var absH = Math.Abs(h);
+                Y -= absH;
+                Height += absH;
+            }
+            else
+            {
+                if (isInversedY && currentY < Y + Height)
+                {
+                    Height -= currentY - Y;
+                    Y = currentY;
+                }
+                else
+                {
+                    isInversedY = false;
+                    Height = h;
+                }
+            }
         }
     }
 }
